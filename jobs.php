eb1d47d233c299a745fee749c22d32ed1234fc17
Mandeep <mandeep.kaur.saini@outlook.com>
2020-03-28 20:54:07 -0400

    first commit

---
+++ jobs.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>JavaJam Coffee House Jobs</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="javajam.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <h1>JavaJam Coffee House</h1>
+        </header>
+        <nav>
+            <a href="index.html">Home</a> &nbsp;
+            <a href="menu.html">Menu</a> &nbsp;
+            <a href="music.html">Music</a> &nbsp;
+            <a href="jobs.html">Jobs</a>
+        </nav>
+        <main>
+            <h2>Jobs at JavaJam</h2>
+            <p>Job opportunities at JavaJam</p>
+            
+                <form action="" method="POST">
+                    <input type="text" id="fname" name="fname" placeholder="First Name" ><br>
+                    <input type="text" id="lname" name="lname" placeholder="Last Name"><br>
+                    <input type="text" id="phone" name="phone" placeholder="Phone"><br>
+                    <input type="text" id="email" name="email" placeholder="Email"><br>
+                    <textarea id="exp" name="exp" placeholder="Experience"></textarea><br>
+                    <input type="submit" value="Apply Here">
+                </form>
+           
+        </main>
+        <footer>
+            Copyright &copy; 2016 JavaJam Coffee House<br>
+            <a href="mailto:yourfirstname@yourlastname.com">yourfirstname@yourlastname.com</a>
+        </footer>
+    </div>
+</body>
+
+</html>
